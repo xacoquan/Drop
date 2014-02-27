b4cc67fc75536dcd813821e5014351c1e448cb7f
Florian FOURATIER <ffourati@e2r13p7.42.fr>
2014-02-27 02:02:29 +0100

    Added tlist_search

---
+++ lib/libft/tlist_search.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tlist_search.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ffourati <ffourati@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/02/27 01:56:10 by ffourati          #+#    #+#             */
+/*   Updated: 2014/02/27 02:02:10 by ffourati         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_tlist_node		*ft_tlist_search(t_tlist *lst, t_tlist_pred predicate)
+{
+	t_tlist_node	*elem;
+
+	elem = lst->elements;
+	while (predicate(elem))
+		elem = elem->next;
+	return (elem);
+}
