0dbfd1c853a1f076599a126e8033f2c25f004fff
Florian FOURATIER <ffourati@e2r13p7.42.fr>
2014-02-27 02:49:45 +0100

    Added tlist_remove_if

---
+++ lib/libft/tlist_remove_if.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tlist_remove_if.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ffourati <ffourati@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/02/27 02:24:36 by ffourati          #+#    #+#             */
+/*   Updated: 2014/02/27 02:45:01 by ffourati         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "libft.h"
+
+void				ft_tlist_remove_if(t_tlist *lst,
+										t_tlist_pred *pred,
+										t_tlist_dtor dtor)
+{
+	t_tlist_node	*node;
+	t_tlist_node	*nxt;
+
+	node = lst->elements;
+	while (node)
+	{
+		nxt = node->next;
+		if (pred(node))
+			ft_tlist_remove(lst, node, dtor);
+		node = nxt;
+	}	
+}
