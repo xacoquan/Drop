8aec681b01bc5a050cc6b75fa51c350edffb3187
Florian FOURATIER <ffourati@e2r13p7.42.fr>
2014-02-27 01:39:10 +0100

    Added tlist_new

---
+++ lib/libft/tlist_new.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tlist_new.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ffourati <ffourati@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/02/27 01:32:27 by ffourati          #+#    #+#             */
+/*   Updated: 2014/02/27 01:38:44 by ffourati         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+t_tlist			*ft_tlist_new(t_list_dtor destructor)
+{
+	t_tlist		*fresh;
+
+	if (!(fresh = (t_list *)malloc(sizeof(t_list))))
+		return (NULL);
+	fresh->elements = NULL;
+	fresh->dtor = destrutor;
+	return (fresh);
+}
