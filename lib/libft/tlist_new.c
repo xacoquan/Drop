1ba8ef84615ddc663a86f7f7c56dc5d523832f50
Florian FOURATIER <ffourati@e2r13p7.42.fr>
2014-02-27 01:43:32 +0100

    Added missing include

--- lib/libft/tlist_new.c
+++ lib/libft/tlist_new.c
@@ -6,11 +6,12 @@
 /*   By: ffourati <ffourati@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/02/27 01:32:27 by ffourati          #+#    #+#             */
-/*   Updated: 2014/02/27 01:38:44 by ffourati         ###   ########.fr       */
+/*   Updated: 2014/02/27 01:43:23 by ffourati         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include "libft.h"
 
 t_tlist			*ft_tlist_new(t_list_dtor destructor)
 {
