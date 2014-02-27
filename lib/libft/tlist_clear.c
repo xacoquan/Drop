21e84968828047b3d024944060991cbcca5038d7
Florian FOURATIER <ffourati@e2r13p7.42.fr>
2014-02-27 02:50:00 +0100

    Added hbfhhhfhfasdlflkakljsafakl;sfjbcvx klfjd

---
+++ lib/libft/tlist_clear.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tlist_clear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ffourati <ffourati@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/02/27 02:24:36 by ffourati          #+#    #+#             */
+/*   Updated: 2014/02/27 02:47:02 by ffourati         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "libft.h"
+
+void				ft_tlist_clear(t_tlist *lst, t_tlist_dtor dtor)
+{
+	t_tlist_node	*node;
+	t_tlist_node	*nxt;
+
+	node = lst->elements;
+	while (node)
+	{
+		nxt = node->next;
+		ft_tlist_remove(lst, node, dtor);
+		node = nxt;
+	}	
+}
